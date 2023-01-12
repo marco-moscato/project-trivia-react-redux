6072955eabf2e9b5e04865714087e533540e4d68
Reivan Chagas <chagasreivan@gmail.com>
2023-01-12 14:50:23 -0300

    adiciona requisição api

---
+++ src/services/api.js
@@ -0,0 +1,15 @@
+export const apiTrivia = async () => {
+  const response = await fetch('https://opentdb.com/api_token.php?command=request');
+  const json = await response.json();
+  const { token } = json;
+  return token;
+};
+
+export const tQuestions = async () => {
+  const token = await apiTrivia();
+  const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+  const json = await response.json();
+  return json.results;
+};
+
+export default tQuestions;
