554fc124e91efe092e8d40a17787ba695f6f25a0
Vitor Wagner <vitormwagner@gmail.com>
2023-01-13 18:39:30 -0300

    Alterações com shuffle

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -29,6 +29,11 @@ class Game extends Component {
     });
   };
 
+  shuffle = (array) => {
+    const random = 0.5;
+    return array.sort(() => Math.random() - random);
+  };
+
   render() {
     const { response, questionIndex, isLoading } = this.state;
     return (
@@ -38,21 +43,30 @@ class Game extends Component {
         <div>
           {
             isLoading ? 'carregando'
-              : <>
-                <h2 data-testid="question-category">
-                  {response[questionIndex].category}
-                </h2>
-                <h2 data-testid="question-text">{response[questionIndex].question}</h2>
-                {console.log(response[questionIndex].incorrect_answers)}
-                {response[questionIndex].incorrect_answers.map()}
-                <button type="button">{response[questionIndex].correct_answer}</button>
-                <button
-                  type="button"
-                  onClick={ this.handleNext }
-                >
-                  Next
-                </button>
-              </>
+              : (
+                <>
+                  <h2 data-testid="question-category">
+                    {response[questionIndex].category}
+                  </h2>
+                  <h2 data-testid="question-text">{response[questionIndex].question}</h2>
+                  {console.log(response[questionIndex].incorrect_answers)}
+                  {this.shuffle(response[questionIndex].incorrect_answers)
+                    .map((answer, index) => (
+                      <button
+                        type="button"
+                        key={ index }
+                      >
+                        {answer}
+                      </button>))}
+                  <button type="button">{response[questionIndex].correct_answer}</button>
+                  <button
+                    type="button"
+                    onClick={ this.handleNext }
+                  >
+                    Next
+                  </button>
+                </>
+              )
           }
 
         </div>
